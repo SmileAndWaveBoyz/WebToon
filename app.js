f48849d73fa4374d1f3f785248568c0672427183
Nathan Caney <nathancaney@gmail.com>
2023-02-05 17:38:58 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require ("express");
+const ejs = require("ejs");
+const app = express();
+ 	
+app.set("view engine", "ejs");
+app.use(express.static("public"));//This allows the css because it's in the public folder
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/about", function(req, res){
+    res.render("about");
+});
+
+app.get("/episodes", function(req, res){
+    res.render("episodes");
+});
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+});
